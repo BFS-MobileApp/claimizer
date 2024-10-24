c43ee3dfb51507ae56a7251e90d1e38ffb3f826d
a7med <a7med.assem@gmail.com>
2024-10-24 11:39:25 +0300

    - Fix Login bug

--- lib/helper.dart
+++ lib/helper.dart
@@ -16,14 +16,24 @@ class Helper{
           now.year, now.month, now.day, startTime.hour, startTime.minute);
       DateTime endDateTime = DateTime(
           now.year, now.month, now.day, endTime.hour, endTime.minute);
-      if (endDateTime.isBefore(startDateTime)) {
+      bool startIsAM = startTime.hour >= 12; // Before 12 PM
+      bool endIsPM = endTime.hour < 12;
+      /*if (endDateTime.isBefore(startDateTime)) {
+        print('yes before');
+        startDateTime = startDateTime.subtract(Duration(days: 1));
+      }*/
+      if(startIsAM && endIsPM){
         endDateTime = endDateTime.add(Duration(days: 1));
       }
       print('Start DateTime: $startDateTime');
       print('End DateTime: $endDateTime');
-      if (now.isBetween(from: startDateTime, to: endDateTime)) {
+      print(now.isAfter(startDateTime));
+      print(now.isBefore(endDateTime));
+      if (now.isAfter(startDateTime) && now.isBefore(endDateTime)) {
+        print('true');
         return true;
       }
+      print('false');
       return false;
     } catch (e) {
       print(e.toString());
